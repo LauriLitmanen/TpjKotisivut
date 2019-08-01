00fb477acf0d51fbcd3daa8d21efdb2750f1f827
LauriLitmanen <litmanen.lauri@gmail.com>
2019-08-01 12:19:49 +0300

    [MATCH RESULTS] Get JSON data from flask_match_app located in api.tpj.fi and construct results list with javascript

--- _site/assets/javascript/main.js
+++ _site/assets/javascript/main.js
@@ -4,12 +4,12 @@ $(function(){ //On page load function
   $(window)
 
   //-----------------NAVBAR ONSCROLL------------------
-  
+
   window.onscroll = function() {myFunction()};
 
   var header = document.getElementById("nav-wrapper");
   var sticky = header.offsetTop;
-  
+
   function myFunction() {
     if (window.pageYOffset > sticky) {
       header.classList.add("fixed");
@@ -84,8 +84,57 @@ $(function(){ //On page load function
         }
       }
     });
+
+  //-------------------------MATCH LIST JSON AND CREATION------------------------------------------
+
+    var xhr = new XMLHttpRequest();                         //#1 create XMLHttpReaquest object
+
+    xhr.onload =  function() {                              //#4 when readystate changes
+      if(xhr.status == 200) {                               //#5 if server status was OK
+        responseObject = JSON.parse(xhr.responseText);      //convert the string to javascript object and store it to a variable
+        var newMatchResult = '';                            // variable to hold the new HTML data
+        for (var i = 0; i < 8; i++) {                       // loop through first 8 matches
+          if (responseObject[i].outCome == 'win') {         // check if the outCome is win or loss
+            matchColor = 'chartreuse';
+          }
+            else {
+              matchColor = 'lightcoral';
+          }                                                //constructing the html with the json data
+          newMatchResult += '<li data-aos="flip-right" data-aos-anchor-placement="center-bottom">';
+          newMatchResult += '<button class="match-item" style="background-color:' + matchColor + ';">';
+          newMatchResult += "BO" + responseObject[i].bestOf + " " + "TPJ vs " + responseObject[i].enemy + " " + responseObject[i].score;
+          newMatchResult += '</button>';
+          newMatchResult += '<div class="match-info"><b>' + responseObject[i].league + '</b><br>';
+          newMatchResult += responseObject[i].mapOne + '<br>' + responseObject[i].mapTwo + '<br>' + responseObject[i].mapThree + '<br>';
+          newMatchResult += '</div>';
+
+          document.getElementById('results-list').innerHTML = newMatchResult; //add the new HTML data to the page
+        }
+        var newMatchResult = '';                            // clear data
+        for (var i = 8; i < responseObject.length; i++) {   //loop trough the rest of the matches
+          if (responseObject[i].outCome == 'win') {         // check if the outCome is win or loss
+            matchColor = 'chartreuse';
+          }
+            else {
+              matchColor = 'lightcoral';
+          }                                                 //constructing the html with the json data
+          newMatchResult += '<li data-aos="flip-right" data-aos-anchor-placement="center-bottom">';
+          newMatchResult += '<button class="match-item" style="background-color:' + matchColor + ';">';
+          newMatchResult += "BO" + responseObject[i].bestOf + " " + "TPJ vs " + responseObject[i].enemy + " " + responseObject[i].score;
+          newMatchResult += '</button>';
+          newMatchResult += '<div class="match-info"><b>' + responseObject[i].league + '</b><br>';
+          newMatchResult += responseObject[i].mapOne + '<br>' + responseObject[i].mapTwo + '<br>' + responseObject[i].mapThree + '<br>';
+          newMatchResult += '</div>';
+
+          document.getElementById('piilotettu').innerHTML = newMatchResult; //add the HTML data to the hidden ul
+        }
+      }
+    }
+    xhr.open ('GET', 'http://api.tpj.fi/api/data', true); //#2 Prepare the request
+    xhr.send(null);                                       //#3 Send the request
 });
-//------------------------ MATCH LIST --------------------------------------------
+
+//------------------------ MATCH LIST MORE INFO --------------------------------------------
 $('.match-list').on('click', '.match-item', function(e){
   e.preventDefault();
   $(this).next('.match-info').not('animated').slideToggle();
@@ -98,7 +147,7 @@ function showMore() {
   var $showMoreText = $('.show-more-text');
   var $arrowUp = $('.up');
   var $arrowDown = $('.down');
-  
+
   if ($matches.is(':hidden')) {
     $matches.slideDown();
     $showMoreText.text("Show Less");
@@ -123,7 +172,3 @@ function toggleNav() {
     nav.style.width = "250px";
   }
 }
-/*
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}*/
